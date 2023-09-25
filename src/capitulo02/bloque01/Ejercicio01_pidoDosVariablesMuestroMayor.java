3ab5a4694c231af23b77fb00a040ded4708bd933
Tinto ★ <Tinto @Tinto>
2023-09-25 18:08:30 +0200

    feat(chapter2): Exercises 1 to 5 added

---
+++ src/capitulo02/bloque01/Ejercicio01_pidoDosVariablesMuestroMayor.java
@@ -0,0 +1,23 @@
+package capitulo02.bloque01;
+
+import java.util.Scanner;
+
+public class Ejercicio01_pidoDosVariablesMuestroMayor {
+
+	public static void main(String[] args) {
+		double num1, num2;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca el valor de la primera variable: ");
+		num1= sc.nextDouble();
+		System.out.println("Introduzca el valor de la segunda variable: ");
+		num2= sc.nextDouble();
+		
+		if(num1>=num2) {
+			System.out.println("El número mayor es: " + num1);
+		}else {
+			System.out.println("El número mayor es: " +num2);
+		}
+	}
+
+}
---
+++ src/capitulo02/bloque01/Ejercicio03_pidoCincoVariablesMuestroMayor.java
@@ -0,0 +1,38 @@
+package capitulo02.bloque01;
+
+import java.util.Scanner;
+
+public class Ejercicio03_pidoCincoVariablesMuestroMayor {
+
+	public static void main(String[] args) {
+		double num1, num2, num3, num4, num5, mayor;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca el valor de la primera variable: ");
+		num1= sc.nextDouble();
+		System.out.println("Introduzca el valor de la segunda variable: ");
+		num2= sc.nextDouble();
+		System.out.println("Introduzca el valor de la tercera variable: ");
+		num3= sc.nextDouble();
+		System.out.println("Introduzca el valor de la cuarta variable: ");
+		num4= sc.nextDouble();
+		System.out.println("Introduzca el valor de la quinta variable: ");
+		num5= sc.nextDouble();
+		mayor=num1;
+		
+		if(mayor<=num2) {
+			mayor=num2;
+		}
+		if(mayor<=num3) {
+			mayor=num3;
+		}
+		if(mayor<=num4) {
+			mayor=num4;
+		}
+		if(mayor<=num5) {
+			mayor=num5;
+		}
+		System.out.println("El número mayor es: " + mayor);
+	}
+
+}
