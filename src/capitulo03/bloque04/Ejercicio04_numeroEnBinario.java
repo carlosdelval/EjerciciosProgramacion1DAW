cb3ba613646ce60a967d13bd4accb4af6733659d
Tinto ★ <Tinto @Tinto>
2023-10-09 19:32:47 +0200

    feat(chapter03): exercise 04 bloque04 added

--- src/capitulo03/bloque04/Ejercicio04_numeroEnBinario.java
+++ src/capitulo03/bloque04/Ejercicio04_numeroEnBinario.java
@@ -1,9 +1,26 @@
 package capitulo03.bloque04;
 
+import java.util.Scanner;
+
 public class Ejercicio04_numeroEnBinario {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num, digito, binario = 0, potencia = 1;
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Introduzca el número que desea convertir en binario:");
+		num = sc.nextInt();
+
+		while (num > 0) {
+			digito = num % 2;
+			binario = digito * potencia + binario;
+			potencia *= 10;
+			num /= 2;
+		}
+
+		System.out.println("El número en formato binario es " + binario);
 
 	}
 
